94edefed6b2d005f1771277983054ce72571f341
hzruanhy <hzruanhy@corp.netease.com>
2014-07-24 10:50:36 +0800

    modify

---
+++ ddt_source/ddtWeb/src/main/webapp/js/slide-banner.js
@@ -0,0 +1,53 @@
+/**
+ * Created by yougen.zhuangyg on 2014/7/23.
+ */
+(function(){
+   var Slide = function(){
+       this.initialize.apply(this, arguments);
+   };
+
+   Slide.prototype = {
+       constructor: Slide,
+       initialize: function(options){
+           this.options = options || {};
+           this.prev =  $(options.prev || "#J-prevTheme");
+           this.next = $(options.next || "#J-nextTheme");
+           this.slideContainer = $(options.slideContainer || "#J-slideContainer");
+           this.slideTarget = $(options.slideTarget || "#J-slideTarget");
+           this.slides = options.slides || [];
+           this.targets = options.targets || [];
+           this.currentSlide = 0;
+           this.installListeners();
+       },
+       installListeners:function(){
+           this.prev.on("click", $.proxy(this.navSlide, this));
+           this.next.on("click", $.proxy(this.navSlide, this));
+       },
+       slide:function(slide){
+           this.slideContainer.css("background-image",'url(' + slide + ')');
+           this.slideTarget.attr("href", this.targets[this.currentSlide]);
+       },
+       navSlide: function(e){
+          e.preventDefault();
+          var deep = $(e.currentTarget)[0] === this.prev[0] ? -1: 1;
+          this.currentSlide += deep;
+          this.currentSlide = this.currentSlide % 3;
+          if(this.currentSlide <0){
+              this.currentSlide += this.slides.length;
+          }
+          this.slide(this.slides[this.currentSlide]);
+       }
+   };
+
+   new Slide({
+       slides: [
+                   "/images/fengxinzi.jpg",
+                   "/images/tangyiwen.jpg"
+       ],
+       targets: [
+                 "www.huajiatangyiwen.com",
+                 "www.huajiatangyiwen.com",
+                 "www.huajiatangyiwen.com"
+       ]
+   });
+})();
